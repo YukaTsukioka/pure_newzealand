1b5ad4cf98ac4cb558db6d415245e4439dd10130
YukaTsukioka <yuka.tsukioka720@gmail.com>
2022-07-10 02:18:29 +0900

    詳細機能の設定

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   
   root to: "posts#index"
 
-  resources :posts, only:[:index, :new, :create, :delete]
+  resources :posts, only:[:index, :new, :create, :delete, :show]
 
 end
