bcb7dd93ebe80e48e5cd2ed444d2948de76e8c4c
YukaTsukioka <yuka.tsukioka720@gmail.com>
2022-07-09 18:06:15 +0900

    投稿機能モデル単体テストコード実行

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,5 +1,13 @@
 FactoryBot.define do
   factory :post do
-    
+    title        {"綺麗なニュージーランド"}
+    text         {"素敵な風景"}
+
+    after(:build) do |post|
+      post.image.attach(io: File.open('spec/images/テスト用の画像.png'), filename: 'テスト用の画像.png' )
+    end
+
+    association :user
+
   end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,43 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @post = FactoryBot.build(:post)
+  end
+  
+  describe '投稿情報保存' do
+    context '投稿情報が保存できるとき' do
+      it 'image,title,textが存在すれば保存できる' do
+        expect(@post).to be_valid
+      end
+    end
+    context '投稿情報が保存できないとき' do
+      it 'titleが空では登録できない' do
+        @post.title = '' 
+        @post.valid?
+       
+        expect(@post.errors.full_messages).to include("Title can't be blank")
+      end
+      it 'textが空では登録できない' do
+        @post.text = '' 
+        @post.valid?
+        
+        expect(@post.errors.full_messages).to include("Text can't be blank")
+      end
+      it 'imageが空では登録できない' do
+        @post.image = nil
+        @post.valid?
+       
+
+        expect(@post.errors.full_messages).to include("Image can't be blank")
+      end
+
+    end
+
+
+
+  end
 end
+
+  
+
