521c7258dd5f7eb5eb1f451d2bf21db8f4def637
YukaTsukioka <yuka.tsukioka720@gmail.com>
2022-07-28 23:20:09 +0900

    アソシエーションの定義

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
 
   has_one_attached :image
   belongs_to :user
+  has_many :comments
 
   validates :image, presence: true
   validates :title, presence: true
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,7 @@ class User < ApplicationRecord
 
          validates :nickname, presence: true 
          PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-         validates_format_of :password, with: PASSWORD_REGEX, message: "Include half-width characters"     
+         validates_format_of :password, with: PASSWORD_REGEX, message: "Include half-width characters"   
+         
+  has_many :comments 
 end
